5ee0b8657054dc8e335d1a0b3b752c5db9d9743f
Sink1234 <100720133+Sink1234@users.noreply.github.com>
2024-02-25 23:14:46 +0300

    Create middlewere.tsx

---
+++ client/src/middlewere.tsx
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { EnumTokens } from "./shared/api/services/auth-token.service";
+import { DASHBOARD_PAGE } from "./config/pages-url.config";
+
+
+export async function middleware(
+    request: NextRequest,
+    response: NextResponse
+) {
+    const {url, cookies} = request
+
+    const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+
+    const isDashboardPage = url.includes('/i')
+    const isAuthPage = url.includes('/auth')
+
+    if(isAuthPage && refreshToken){
+        return NextResponse.redirect(new URL(DASHBOARD_PAGE.HOME, url))
+    }
+
+    if(isAuthPage){
+        return NextResponse.next()
+    }
+
+    if(!refreshToken){
+        return NextResponse.redirect(new URL('/auth', url))
+    }
+
+    return
+}
+
+export const config = {
+    matcher: ['/i/:path*', '/auth/:path']
+}
